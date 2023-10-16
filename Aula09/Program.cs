18e94d311161dc509ad9ae3afe9cd7fd5d298d53
Michael <Michael@DESKTOP-61L2M0C>
2023-10-16 19:47:36 -0300

    Adicionar arquivos de projeto.

---
+++ Aula09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 2;
+            num = num << 2;
+            Console.WriteLine(num);
+
+
+        }
+    }
+}
